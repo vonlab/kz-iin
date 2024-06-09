aa4d670aa6b2b39872b68706e41f6e6f8053f135
Von Lab <vonlab@yahoo.com>
2024-06-09 15:51:27 +0300

    test: add test generate() with given birth date and gender

--- tests/IinGeneratorTest.php
+++ tests/IinGeneratorTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace VonLab\KzIin\Tests;
 
+use Mockery;
 use PHPUnit\Framework\TestCase;
 use VonLab\KzIin\Data\BirthDate;
 use VonLab\KzIin\Enums\GenderEnum;
@@ -13,6 +14,14 @@ use VonLab\KzIin\Utils\IinHelper;
 
 class IinGeneratorTest extends TestCase
 {
+    /**
+     * @return void
+     */
+    public function tearDown(): void
+    {
+        Mockery::close();
+    }
+
     /**
      * @throws InvalidIinFormatException
      */
@@ -24,11 +33,47 @@ class IinGeneratorTest extends TestCase
         $gender = GenderEnum::Male;
         $iin = $generator->generate($birthDate, $gender);
 
+        $this->assertIsString($iin);
+        $this->assertEquals(12, strlen($iin));
+
         $this->assertTrue(IinHelper::isValidControlDigit($iin));
 
         $this->assertSame('90', substr($iin, 0, 2));
         $this->assertSame('01', substr($iin, 2, 2));
         $this->assertSame('01', substr($iin, 4, 2));
         $this->assertSame('3', $iin[6]);  // Century digit for 1990s and male
+    }
+
+    /**
+     * @throws InvalidIinFormatException
+     * @return void
+     */
+    public function testGenerateWithoutParameters()
+    {
+        $generatorMock = Mockery::mock(IinGenerator::class)->makePartial();
+
+        $birthDate = new BirthDate(1990, 1, 1);
+        $gender = GenderEnum::Male;
+
+        $generatorMock->shouldAllowMockingProtectedMethods();
+        $generatorMock->shouldReceive('generateRandomBirthDate')
+            ->once()
+            ->andReturn($birthDate);
+
+        $generatorMock->shouldReceive('generateRandomGender')
+            ->once()
+            ->andReturn($gender);
+
+        $iin = $generatorMock->generate();
+
+        $this->assertIsString($iin);
+        $this->assertEquals(12, strlen($iin));
+
+        $this->assertTrue(IinHelper::isValidControlDigit($iin));
+
+        $this->assertEquals('90', substr($iin, 0, 2));
+        $this->assertEquals('01', substr($iin, 2, 2));
+        $this->assertEquals('01', substr($iin, 4, 2));
+        $this->assertSame('3', $iin[6]);
     }
 }
